e2f3e17db90ffd37b9b4fb8b0cb6debf488d5cb3
Maksim Chemerisuk <chemerisuk@gmail.com>
2013-12-25 09:56:02 +0300

    improve fast click support

--- src/better-ajaxify.js
+++ src/better-ajaxify.js
@@ -37,7 +37,17 @@
                     // update page title
                     DOM.set("title", response.title);
                 };
-            }());
+            }()),
+            handleLinkClick = function(link, cancel) {
+                if (!cancel && !link.get("target") && !link.get("href").indexOf("http")) {
+                    return !link.fire("ajaxify:fetch");
+                }
+            },
+            handleFormSubmit = function(form, cancel) {
+                if (!cancel && !form.get("target")) {
+                    return !form.fire("ajaxify:fetch");
+                }
+            };
 
         DOM.on("ajaxify:fetch", (function() {
             // lock element to prevent double clicks
@@ -145,37 +155,25 @@
             // http://updates.html5rocks.com/2013/12/300ms-tap-delay-gone-away
             if (~el.get("content").indexOf("width=device-width")) {
                 // fastclick support via triggering some events earlier
-                DOM.on("touchend a", function(link) {
-                    link.fire("click");
-
-                    return false;
-                });
-
-                DOM.on("touchend [type=submit]", function(btn) {
-                    btn.parent("form").fire("submit");
-
-                    return false;
+                DOM.on("touchend", function(el, cancel) {
+                    if (el.matches("a")) {
+                        return handleLinkClick(el, cancel);
+                    } else if (el.get("type") === "submit") {
+                        return handleFormSubmit(el.parent("form"), cancel);
+                    }
                 });
             }
         });
 
-        DOM.on("click a", function(link, cancel) {
-            if (!cancel && !link.get("target") && !link.get("href").indexOf("http")) {
-                return !link.fire("ajaxify:fetch");
-            }
-        });
-
-        DOM.on("submit", function(form, cancel) {
-            if (!cancel && !form.get("target")) {
-                return !form.fire("ajaxify:fetch");
-            }
-        });
-
-        DOM.on("ajaxify:history", function(url) {
-            if (url in historyData) {
-                switchContent(historyData[url]);
-            } else {
-                DOM.fire("ajaxify:fetch", url);
+        DOM.on({
+            "click a": handleLinkClick,
+            "submit": handleFormSubmit,
+            "ajaxify:history": function(url) {
+                if (url in historyData) {
+                    switchContent(historyData[url]);
+                } else {
+                    DOM.fire("ajaxify:fetch", url);
+                }
             }
         });
     });
