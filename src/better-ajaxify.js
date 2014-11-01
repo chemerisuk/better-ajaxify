81408783474bdebb03b6d96eb92156f0902c5759
Maksim Chemerisuk <chemerisuk@gmail.com>
2014-11-01 12:14:59 +0300

    remove hashchange support

--- src/better-ajaxify.js
+++ src/better-ajaxify.js
@@ -141,6 +141,32 @@
         }
     });
 
+    /* istanbul ignore else */
+    if (history.pushState) {
+        window.addEventListener("popstate", function(e) {
+            if (e.state) {
+                DOM.fire("ajaxify:history", location.href);
+            }
+        });
+        // update initial state address url
+        history.replaceState(true, DOM.get("title"));
+        // fix bug with external pages
+        window.addEventListener("beforeunload", function() {
+            history.replaceState(null, DOM.get("title"));
+        });
+    } else {
+        // when url should be changed don't start request in old browsers
+        DOM.on("ajaxify:loadstart", ["target", "defaultPrevented"], function(sender, canceled) {
+            if (canceled) return;
+            // load a new page in legacy browsers
+            if (sender.matches("form")) {
+                sender.fire("submit");
+            } else if (sender.matches("a")) {
+                location.href = sender.get("href");
+            }
+        });
+    }
+
     DOM.extend("form", {
         constructor: function() {
             var submits = this.findAll("[type=submit]");
