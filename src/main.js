c9645da445ad7ecf10555e4cccba94ae0b79c3b4
Maksim Chemerisuk <chemerisuk@gmail.com>
2017-02-26 08:44:58 +0300

    extract attachNonPreventedListener, update demo

--- src/main.js
+++ src/main.js
@@ -6,6 +6,14 @@
     const states = []; // in-memory storage for states
     var currentState = {};
 
+    function attachNonPreventedListener(eventType, callback) {
+        document.addEventListener(eventType, function(e) {
+            if (!e.defaultPrevented) {
+                callback(e);
+            }
+        }, false);
+    }
+
     function dispatchAjaxifyEvent(el, eventType, eventDetail) {
         const e = document.createEvent("CustomEvent");
 
@@ -26,80 +34,76 @@
         document.title = title;
     }
 
-    document.addEventListener("click", function(e) {
-        if (!e.defaultPrevented) {
-            const el = e.target;
+    attachNonPreventedListener("click", (e) => {
+        const el = e.target;
 
-            var link;
+        var link;
 
-            if (el.nodeName.toLowerCase() === "a") {
-                // detected click on a link
-                link = el;
-            } else {
-                const focusedElement = document.activeElement;
+        if (el.nodeName.toLowerCase() === "a") {
+            // detected click on a link
+            link = el;
+        } else {
+            const focusedElement = document.activeElement;
 
-                if (focusedElement.nodeName.toLowerCase() === "a") {
-                    if (focusedElement.contains(el)) {
-                        // detected click on a link inner element
-                        link = focusedElement;
-                    }
-                }
-            }
-
-            if (link && !link.target) {
-                // handle only http(s) links
-                if (link.protocol.slice(0, 4) === "http") {
-                    const targetUrl = link.href;
-                    const currentUrl = location.href;
-
-                    if (targetUrl === currentUrl || targetUrl.split("#")[0] !== currentUrl.split("#")[0]) {
-                        if (dispatchAjaxifyEvent(link, "fetch", link.href)) {
-                            // override default bahavior for links
-                            e.preventDefault();
-                        }
-                    } else {
-                        location.hash = link.hash;
-                        // override default bahavior for anchors
-                        e.preventDefault();
-                    }
+            if (focusedElement.nodeName.toLowerCase() === "a") {
+                if (focusedElement.contains(el)) {
+                    // detected click on a link inner element
+                    link = focusedElement;
                 }
             }
         }
-    }, false);
 
-    document.addEventListener("submit", function(e) {
-        if (!e.defaultPrevented) {
-            const el = e.target;
+        if (link && !link.target) {
+            // handle only http(s) links
+            if (link.protocol.slice(0, 4) === "http") {
+                const targetUrl = link.href;
+                const currentUrl = location.href;
 
-            if (!el.target && dispatchAjaxifyEvent(el, "fetch", el.action)) {
+                if (targetUrl === currentUrl || targetUrl.split("#")[0] !== currentUrl.split("#")[0]) {
+                    if (dispatchAjaxifyEvent(link, "fetch", link.href)) {
+                        // override default bahavior for links
+                        e.preventDefault();
+                    }
+                } else {
+                    location.hash = link.hash;
+                    // override default bahavior for anchors
+                    e.preventDefault();
+                }
+            }
+        }
+    });
+
+    attachNonPreventedListener("submit", (e) => {
+        const el = e.target;
+
+        if (!el.target) {
+            if (dispatchAjaxifyEvent(el, "fetch", el.action)) {
                 e.preventDefault();
             }
         }
-    }, false);
+    });
 
-    document.addEventListener("ajaxify:fetch", function(e) {
-        if (!e.defaultPrevented) {
-            const el = e.target;
-            const xhr = new XMLHttpRequest();
+    attachNonPreventedListener("ajaxify:fetch", (e) => {
+        const el = e.target;
+        const xhr = new XMLHttpRequest();
 
-            ["abort", "error", "load", "timeout"].forEach((type) => {
-                xhr["on" + type] = () => {
-                    dispatchAjaxifyEvent(el, type, xhr);
-                };
-            });
+        ["abort", "error", "load", "timeout"].forEach((type) => {
+            xhr["on" + type] = () => {
+                dispatchAjaxifyEvent(el, type, xhr);
+            };
+        });
 
-            xhr.open((el.method || "get").toUpperCase(), e.detail, true);
-            xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
-            xhr.responseType = "document";
-            xhr.data = null;
+        xhr.open((el.method || "get").toUpperCase(), e.detail, true);
+        xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+        xhr.responseType = "document";
+        xhr.data = null;
 
-            if (dispatchAjaxifyEvent(el, "send", xhr)) {
-                xhr.send(xhr.data);
-            }
+        if (dispatchAjaxifyEvent(el, "send", xhr)) {
+            xhr.send(xhr.data);
         }
-    }, false);
+    });
 
-    document.addEventListener("ajaxify:load", function(e) {
+    attachNonPreventedListener("ajaxify:load", (e) => {
         const xhr = e.detail;
         const res = xhr.response;
         const resBody = res.body;
@@ -116,30 +120,28 @@
             }
         }
 
-        if (dispatchAjaxifyEvent(el, "replace", replacement)) {
-            const url = res.URL || xhr.responseURL;
-            const title = res.title || document.title;
+        dispatchAjaxifyEvent(el, "replace", replacement);
 
-            updateCurrentState(el, title);
+        const url = res.URL || xhr.responseURL;
+        const title = res.title || document.title;
 
-            if (url !== location.href) {
-                history.pushState(states.length, title, url);
-            }
+        updateCurrentState(el, title);
 
-            currentState = {}; // create a new state object
+        if (url !== location.href) {
+            history.pushState(states.length, title, url);
         }
-    }, false);
+
+        currentState = {}; // create a new state object
+    });
 
     // default behavior for a content replacement
-    document.addEventListener("ajaxify:replace", function(e) {
-        if (!e.defaultPrevented) {
-            const el = e.target;
+    attachNonPreventedListener("ajaxify:replace", (e) => {
+        const el = e.target;
 
-            el.parentNode.replaceChild(e.detail, el);
-        }
-    }, false);
+        el.parentNode.replaceChild(e.detail, el);
+    });
 
-    window.addEventListener("popstate", function(e) {
+    window.addEventListener("popstate", (e) => {
         var stateIndex = e.state;
         // numeric value indicates better-ajaxify state
         if (stateIndex >= 0) {
@@ -147,7 +149,9 @@
             const id = state.el.id;
             const el = id ? document.getElementById(id) : document.body;
 
-            if (el && dispatchAjaxifyEvent(el, "replace", state.el)) {
+            if (el) {
+                dispatchAjaxifyEvent(el, "replace", state.el);
+
                 updateCurrentState(el, state.title);
 
                 currentState = state;
