1882d2e2553bba4d37df0089ebd2f1b29878a599
Maksim Chemerisuk <chemerisuk@gmail.com>
2020-04-10 13:47:54 +0300

    add spinner to the demo

--- demo/demo.js
+++ demo/demo.js
@@ -1,3 +1,22 @@
+document.addEventListener("ajaxify:fetch", function(e) {
+    console.log("ajaxify:fetch", e);
+
+    document.documentElement.setAttribute("aria-busy", "true");
+});
+
+document.addEventListener("ajaxify:error", function(e) {
+    console.log("ajaxify:error", e);
+
+    document.documentElement.removeAttribute("aria-busy");
+});
+
+document.addEventListener("ajaxify:navigate", function(e) {
+    console.log("ajaxify:navigate", e);
+
+    document.documentElement.removeAttribute("aria-busy");
+});
+
+
 //document.documentElement.querySelector("main").setAttribute("data-ajaxify", "off");
 //document.documentElement.setAttribute("data-ajaxify", "on");
 
